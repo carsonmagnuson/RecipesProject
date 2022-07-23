85a6082c8362d7b732027722b98bc954008a9b2b
carsonmagnuson <carsonmagnuson@gmail.com>
2022-07-23 12:42:07 -0400

    added basic routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import {Component, EventEmitter, OnInit, Output} from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() showEvent = new EventEmitter<string>();
+  // @Output() showEvent = new EventEmitter<string>();
 
   ngOnInit(): void {
   }
@@ -14,8 +14,8 @@ export class HeaderComponent implements OnInit {
   constructor() {
   }
 
-  onNavClick( item: string) {
-    this.showEvent.emit(item);
-  }
+  // onNavClick( item: string) {
+  //   this.showEvent.emit(item);
+  // }
 
 }
