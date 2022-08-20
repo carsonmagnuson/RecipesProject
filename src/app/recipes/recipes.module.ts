d1ca52866f91b04b7d692d73df4fc2fdeacc1dd1
Carson Magnuson <80717302+carsonmagnuson@users.noreply.github.com>
2022-08-20 14:48:01 -0400

    outsourced shopping list routing config to own module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -22,19 +22,10 @@ import {RecipesRoutingModule} from "./recipes-routing.module";
     RecipeEditComponent,
   ],
   imports: [
-    RouterModule,
     CommonModule,
     ReactiveFormsModule,
     RecipesRoutingModule
   ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-  ]
 })
 export class RecipesModule {
 
