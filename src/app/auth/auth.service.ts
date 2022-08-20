826c7794b9b1a093fb419cfa2eee9e03e90f9372
Carson Magnuson <80717302+carsonmagnuson@users.noreply.github.com>
2022-08-20 17:52:43 -0400

    outsourced fb api key to environment variable

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {catchError, tap} from "rxjs/operators";
 import {BehaviorSubject, Subject, throwError} from "rxjs";
 import {User} from "./user.model";
 import {Router} from "@angular/router";
+import {environment} from "../../environments/environment";
 
 export interface AuthResponseData {
   kind: string;
@@ -25,7 +26,8 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBaOUoq8sHnSLBvmPQZLZOQ9V63Tjhux_4',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+      + environment.firebaseAPIKey,
       {
         email: email,
         password: password,
@@ -45,7 +47,8 @@ export class AuthService {
 
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaOUoq8sHnSLBvmPQZLZOQ9V63Tjhux_4',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+      + environment.firebaseAPIKey,
       {
         email: email,
         password: password,
